11b0528b074f2ad2b1fd2589a71bd8550b889213
Ldj-git <0108dongjun@gmail.com>
2023-04-16 22:12:59 +0900

    python 예시 한글 깨짐 수정

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const iconv = require('iconv-lite');
 const app = express()
 const port = 5000
 
+// word-cloud
 app.get('/word-cloud/keyword/:keyword', (req, res) => {
     console.log(req.params)
+
+    // 자식 프로세스 생성 test.py 실행하고 argv로 url에서 keyword 받은것 보냄
     const result = spawn('python', ['test.py', req.params.keyword])
 
+    // python 실행 결과 나오면 한글로 변환후 
     result.stdout.on('data', function (data) {
         let result = iconv.decode(data, 'euc-kr')
         console.log(result);
-        res.send(`wordcount ${JSON.stringify(req.params)} ${result}`)
+        res.send(`${result}`)
     });
 
     result.stderr.on('data', function (data) {
--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import sys
+import json
 
 def test(x):
-    print(x+"입력 받음")
+    ret={"result":x}
+    # 표준입출력으로 출력된것을 nodejs에서 인식합니다.
+    print(json.dumps(ret,ensure_ascii = False))
 
 if __name__ == '__main__':
+    # sys.argv로 test.py 뒤에 인자 받기
     test(sys.argv[1])
