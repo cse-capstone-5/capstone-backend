68eb8b047f8b802fd0b85c9750ef74c321941459
r3nas <43173420+r3nas@users.noreply.github.com>
2023-05-06 14:44:25 +0900

    json 출력

--- crawl_cent.py
+++ crawl_cent.py
@@ -6,32 +6,30 @@ import datetime
 from tqdm import tqdm
 import sys
 from pororo import Pororo
-import matplotlib.pyplot as plt
 from dateutil.relativedelta import relativedelta
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
 import asyncio
 import aiohttp
 import nest_asyncio
 from konlpy.tag import Okt
+from collections import Counter
 import time
-
-#뷰어 링크
-#https://nbviewer.org/gist/r3nas/e77d37a1e88d6dfcffc6dc78b0275d80
-# %%
-#sent = Pororo(task="sentiment", model="brainbert.base.ko.nsmc", lang="ko")
-sent = Pororo(task="sentiment", model="brainbert.base.ko.shopping", lang="ko")
-okt = Okt()
-page_limit = 1
+from wordcloud import WordCloud
+import json
 
 # %%
-def get_cent(txt):
+def get_sent(txt):
     return sent(txt,show_probs=True)['positive']-0.5
 
 # %%
 def get_noun(txt):
     return okt.nouns(txt)
+
+# %%
+def split_sentence(txt):
+    txt = txt.split(' ')
+    txt = [t for t in txt if t]
+    return txt
 
 # %%
 def str_to_date(d):
@@ -54,19 +52,27 @@ def date_to_str(d):
 
 # %%
 def to_period(ds,de,date_scale):
-    result = []
+    sent_data = []
     dif = relativedelta(days=int(date_scale))
     while(ds<de):
-        result.append(date_to_str(ds))
+        sent_data.append(date_to_str(ds))
         ds = ds+dif
         if(ds>=de):
             result.append(date_to_str(de))
     return result
 
 # %%
+def erase_tag(txt):
+    pattern = '<[^>]*>'
+    txt = re.sub(pattern, repl='', string=str(txt))
+    return txt
+
+# %%
 def clean_txt(txt):
-    erase_tag = '<[^>]*>'
-    txt = re.sub(pattern=erase_tag, repl='', string=str(txt))
+    pattern = '[^ㄱ-ㅎㅏ-ㅣ가-힣0-9\s]'
+    txt = re.sub(pattern, repl=' ', string=str(txt))
+    pattern = '\s+'
+    txt = re.sub(pattern,repl=' ',string=str(txt))
     return txt
 
 # %%
@@ -94,25 +100,32 @@ def date_cal(before):
 
 # %%
 async def get_text(keyword,day):
-    async with aiohttp.ClientSession() as sess:
-        for p in range(page_limit):
-            url = 'https://search.naver.com/search.naver?where=news&query={0}&pd=3&ds={1}&de={2}&start={3}'.format(keyword,day,day,p*10)
-            async with sess.get(url,headers={'user-agent': 'Mozilla/5.0'}) as html:
-                html = await html.text()
+    async with aiohttp.ClientSession() as sess: # aiohttp를 사용하여 비동기적으로 세션을 생성합니다.
+        for p in range(int(limit_per_day/10)): # 페이지 수 만큼 반복합니다.
+            url = 'https://search.naver.com/search.naver?where=news&query={0}&pd=3&ds={1}&de={2}&start={3}'.format(keyword,day,day,p*10) # 검색어, 날짜, 페이지 번호를 이용하여 url을 생성합니다.
+            async with sess.get(url,headers={'user-agent': 'Mozilla/5.0'}) as html: # 생성한 url로 get 요청을 보냅니다.
+                html = await html.text() # 응답받은 html을 text로 변환합니다.
                 try:
-                    html = html[html.index('class="list_news"'):html.index('class="banner_area"')]
+                    html = html[html.index('class="list_news"'):html.index('class="banner_area"')] # 뉴스 리스트가 있는 부분만 추출합니다.
                 except:
-                    continue
-                html = bs(html,"lxml")
-                raw_title = html.select("li div.news_area > a.news_tit")
-                title = [t.attrs['title'] for t in raw_title]
-                num_res = len(raw_title)
+                    continue # 뉴스 리스트가 없으면 다음 페이지로 넘어갑니다.
+                html = bs(html,"lxml") # 추출한 html을 bs4로 파싱합니다.
+                article_info = html.select("li div.news_area > a")
+                titles = [t.attrs['title'] for t in article_info] # 뉴스 제목만 추출합니다.
+                urls = [u.attrs['href'] for u in article_info] 
+                num_res = len(titles) # 추출한 뉴스 수를 저장합니다.
+                
+                tmp = [[t,u] for t,u in zip(titles,urls) if keyword in t]
+                titles,urls = [],[]
 
-                title = [t.attrs['title'] for t in raw_title if keyword in t.attrs['title']]
-                raw_df = pd.DataFrame({'title':title,'date':day})
-                global dataset
-                dataset = pd.concat([raw_df,dataset],ignore_index=True)
-                if(num_res != 10 or len(title)==0):
+                for t in tmp:
+                    titles.append(t[0])
+                    urls.append(t[1])
+
+                raw_df = pd.DataFrame({'title':titles,'url':urls,'date':day}) # 추출한 뉴스 제목과 날짜를 데이터프레임으로 생성합니다.
+                global news_data
+                news_data = pd.concat([raw_df,news_data],ignore_index=True) # 추출한 데이터프레임을 전역 변수 dataset에 추가합니다.
+                if(num_res != 10 or len(titles)==0): # 추출한 뉴스 수가 10개가 아니거나 검색어가 포함된 뉴스가 없으면 반복문을 종료합니다.
                     #print("end of articles")
                     break
 
@@ -125,29 +138,110 @@ async def async_crawl(keyword,ds,de):
     await (asyncio.wait(task))
 
 # %%
-async def main():
-    keyword = "신천지"
-    ds = str_to_date('20200201')
-    de = str_to_date('20200301')
-    await async_crawl(keyword,ds,de)
-
-    dataset['date'] = pd.to_datetime(dataset['date'])
-    dataset['nouns'] = dataset['title'].apply(get_noun)
-    dataset['cent_val'] = dataset['title'].apply(get_cent)
-
-    global result
-    result = dataset.resample(on='date',rule='D').sum()
-    result = result.fillna(0)
-
-    for d in result['cent_val'].index:
-        plist = (dataset['cent_val']>=0)&(dataset['date']==d)
-        nlist = (dataset['cent_val']<0)&(dataset['date']==d)
-        result.loc[d,'npos'] = plist.sum()
-        result.loc[d,'nneg'] = nlist.sum()
-        result.loc[d,'pos'] = dataset[plist]['cent_val'].sum()
-        result.loc[d,'neg'] = dataset[nlist]['cent_val'].sum()
+def word_cloud(df):
+    word = sum(df['split'],[])
+    vocab = Counter(word)
+    vocab = vocab.most_common(vocab_size)
+    wc = {"wordCloud" : dict(vocab)}
+    return json.dumps(wc)
 
 # %%
-dataset = pd.DataFrame(columns=['title','date','cent_val'])
-result = pd.DataFrame()
-asyncio.run(main())
+def get_article():
+    sent_data['num_diff'] = sent_data['sent_val']
+    sent_data['sent_diff'] = sent_data['sent_val'].diff()
+    sent_data['sent_diff'] = sent_data['sent_diff'].apply(abs)
+    posmax = sent_data['num_diff'].max()
+    posday = sent_data.loc[sent_data['num_diff']==posmax]['sent_val'].idxmax()
+    negmax = sent_data['num_diff'].min()
+    negday = sent_data.loc[sent_data['num_diff']==negmax]['sent_val'].idxmax()
+    repday = sent_data['sent_diff'].idxmax()
+    days = [repday,posday,negday]
+    articles = {}
+    
+    for d in days:
+        tmp=get_represent(news_data.loc[news_data['date'] == d])
+        articles[tmp['title']]=tmp['url']
+        
+    return list(articles.items())
+
+# %%
+def get_represent(df):
+    word = sum(df['split'],[])
+    vocab = Counter(word)
+    vocab = vocab.most_common(vocab_size)
+    word_idx = {}
+    i = 1
+    for (word,freq) in vocab:
+        word_idx[word] = i
+        i=i+1
+    
+    for idx in df.index:
+        val = 0
+        for w in df.loc[idx,'split']:
+            if w in word_idx:
+                val = val + word_idx[w]
+            else:
+                val = val + len(word_idx)
+        val = val/len(df.loc[idx,'split'])
+        df.loc[idx,'represent'] = val
+    
+    return df.loc[df['represent'].idxmin()]
+
+# %%
+def get_linechart():
+    lineChart = []
+    for row in sent_data.itertuples():
+        tmp = {"name":str(row[0]),"count":row[1]}
+        lineChart.append(tmp)
+    return lineChart
+
+# %%
+async def crawl_sent_analysis(keyword):
+    #print("crawling")
+    await async_crawl(keyword,ds,de)
+    news_data['date'] = pd.to_datetime(news_data['date'])
+    #print("cleaning text")
+    news_data['title'] = news_data['title'].apply(clean_txt)
+    #print("split sentence")
+    news_data['split'] = news_data['title'].apply(split_sentence)
+    #print("sentment analysis")
+    news_data['sent_val'] = news_data['title'].apply(get_sent)
+    #print("done")
+    
+    global sent_data
+    sent_data = news_data.resample(on='date',rule='D').sum()
+    #sent_data = sent_data.fillna(0)
+
+    for d in sent_data['sent_val'].index:
+        plist = (news_data['sent_val']>=0)&(news_data['date']==d)
+        nlist = (news_data['sent_val']<0)&(news_data['date']==d)
+        sent_data.loc[d,'npos'] = plist.sum()
+        sent_data.loc[d,'nneg'] = nlist.sum()
+        sent_data.loc[d,'pos'] = news_data[plist]['sent_val'].sum()
+        sent_data.loc[d,'neg'] = news_data[nlist]['sent_val'].sum()
+    sent_data['diff'] = sent_data['sent_val'].diff()
+
+    result = {}
+    result["article"] = get_article()
+    result["wordCloud"] = word_cloud(news_data)
+    result["lineChart"] = get_linechart()
+    print(json.dumps(result,ensure_ascii = False))
+
+# %%
+#sent = Pororo(task="sentiment", model="brainbert.base.ko.nsmc", lang="ko")
+sent = Pororo(task="sentiment", model="brainbert.base.ko.shopping", lang="ko")
+okt = Okt()
+limit_per_day = 10
+vocab_size = 100
+news_data = pd.DataFrame(columns=['title','url','date','sent_val'])
+sent_data = pd.DataFrame()
+#keyword = "전세"
+ds = str_to_date('20230401')
+de = str_to_date('20230501')
+
+# %%
+if __name__ == '__main__':
+    #await crawl_sent_analysis(sys.argv[1])
+    asyncio.run(crawl_sent_analysis(sys.argv[1]))
+
+
