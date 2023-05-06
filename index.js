3249853bdfa8355ca104fd4dd66631830d9d9a87
Ldj-git <0108dongjun@gmail.com>
2023-05-06 17:29:29 +0900

    비동기 추가

--- index.js
+++ index.js
@@ -11,14 +11,17 @@ async function getData(keyword) {
     // 자식 프로세스 생성 sentimeter.py 실행하고 argv로 keyword 보냄
     const result = spawn('conda', ['run', "-n", "pororo", "python", "sentimeter.py", keyword])
 
+    //https://stackoverflow.com/questions/58570325/how-to-turn-child-process-spawns-promise-syntax-to-async-await-syntax
+    let data = "";
+    for await (const chunk of result.stdout) {
+        data += chunk;
+    }
     // python 실행 결과 나오면 한글로 변환후 
     //https://validming99.tistory.com/117 한글 깨짐 참고
-    result.stdout.on('data', function (data) {
-        let encoded = data.toString('utf8');
-        // let result = iconv.decode(data, 'euc-kr')
-        console.log(encoded);
-        cache[keyword] = encoded
-    });
+    let encoded = data.toString('utf8');
+    // let result = iconv.decode(data, 'euc-kr')
+    console.log(encoded);
+    cache[keyword] = encoded
 
     result.stderr.on('data', function (data) {
         let encoded = data.toString('utf8');
@@ -33,6 +36,7 @@ app.get('/article/keyword/:keyword', async (req, res) => {
         //캐시 안에 없다면 파이썬 돌림
         await getData(req.params.keyword)
     }
+    console.log(cache[req.params.keyword])
     res.send({ "result": cache[req.params.keyword]['article'] })
 })
 
@@ -43,6 +47,7 @@ app.get('/word-cloud/keyword/:keyword', async (req, res) => {
         //캐시 안에 없다면 파이썬 돌림
         await getData(req.params.keyword)
     }
+    console.log(cache[req.params.keyword])
     res.send({ "result": cache[req.params.keyword]['wordCloud'] })
 })
 
@@ -52,6 +57,7 @@ app.get('/line-chart/keyword/:keyword', async (req, res) => {
         //캐시 안에 없다면 파이썬 돌림
         await getData(req.params.keyword)
     }
+    console.log(cache[req.params.keyword])
     res.send({ "result": cache[req.params.keyword]['lineChart'] })
 })
 
